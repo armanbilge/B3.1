1e481668c14a15e03829980216331e19bf812c6f
Arman Bilge <armanbilge@gmail.com>
2015-06-23 15:01:27 +1200

    Create NonDifferentiableException

---
+++ src/beast/math/NonDifferentiableException.java
@@ -0,0 +1,32 @@
+/*
+ * NonDifferentiableException.java
+ *
+ * BEAST: Bayesian Evolutionary Analysis by Sampling Trees
+ * Copyright (C) 2015 BEAST Developers
+ *
+ * BEAST is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * BEAST is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package beast.math;
+
+/**
+ * Thrown when attempting to differentiate a function at a non-differentiable point.
+ *
+ * @author Arman Bilge
+ */
+public class NonDifferentiableException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+}
